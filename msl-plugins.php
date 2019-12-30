86459e7061477c5aa04e43814e8a6fbe3a7332ee
Gaetanbrl <gaetan.bruel@jdev.fr>
2019-12-30 15:47:40 +0100

    Replace plugin path as consturctor param by const

--- msl-plugins.php
+++ msl-plugins.php
@@ -12,9 +12,12 @@ Author URI: https://jdev.fr
 */
 include('classes/MslPlugin.php');
 
-$msl = new MslPlugin(plugin_dir_url( __FILE__ ));
+$msl = new MslPlugin();
 
 $msl->register();
+
+$url = plugin_dir_url(__FILE__);
+define('MSL_PLUGIN_URL',$url);
 
 register_activation_hook( __FILE__, array( $msl, 'activate' ) );
 register_deactivation_hook( __FILE__, array( $msl, 'deactivate' ) );
